8c8669294cd0d50cf7853aa8d7dbf9217956b359
Eric Uen <ikpassat@gmail.com>
2023-11-24 22:58:08 +0800

    71_API Resources - Controlling JSON Response

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\EventResource;
 use App\Models\Event;
 use Illuminate\Http\Request;
 
@@ -13,7 +14,8 @@ class EventController extends Controller
      */
     public function index()
     {
-        return Event::all();
+        // return EventResource::collection(Event::all());
+        return EventResource::collection(Event::with('user')->get());
     }
 
     /**
@@ -32,7 +34,7 @@ class EventController extends Controller
             ]),
             'user_id' => 1,
         ]);
-        return $event;
+        return new EventResource($event);
     }
 
     /**
@@ -40,7 +42,8 @@ class EventController extends Controller
      */
     public function show(Event $event)
     {
-        return $event;
+        $event->load('user', 'attendees');
+        return new EventResource($event);
     }
 
     /**
@@ -58,7 +61,7 @@ class EventController extends Controller
 
             ])
         );
-        return $event;
+        return new EventResource($event);
     }
 
     /**
--- app/Http/Resources/EventResource.php
+++ app/Http/Resources/EventResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,6 +15,17 @@ class EventResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        // return parent::toArray($request);
+        //* 自行定義回傳的內容陣列需要有哪些欄位
+        return [
+            'id'          => $this->id,
+            'name'        => $this->name,
+            'description' => $this->description,
+            'start_time'  => $this->start_time,
+            'end_time'    => $this->end_time,
+            'user'        => new UserResource($this->whenLoaded('user')),
+            'attendees'   => AttendeeResource::collection($this->whenLoaded('attendees')),
+
+        ];
     }
 }
